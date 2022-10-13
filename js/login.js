7398b3bb0ec43b1651a4dc22d486ab0358c04e88
João Fernandes <113243454+Jpfernandesm@users.noreply.github.com>
2022-10-13 17:04:17 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,11 +1,15 @@
+
+
+
+/* Variáveis criadas para o input de login (ao escrever), botão para submeter e o formulário*/
 const input = document.querySelector(".login__input");
 const button = document.querySelector(".login__button");
 const form = document.querySelector(".login-form");
-/*Desestruturação de objeto*/
 
+/* Desestruturação de objeto, ainda possuo algumas dúvidas e fico confuso ao método ({target}) */
 const validateInput = ({ target }) => {
-  if (target.value.length > 2) {
-    button.removeAttribute("disabled");
+  if (target.value.length > 2) {         /*Caso o tamanho do target value seja maior que dois, executará o button.removeAttribute. */
+    button.removeAttribute("disabled"); /*atributo de remoção*/
     return;
   }
   button.setAttribute("disabled", "");
@@ -16,5 +20,10 @@ const handleSubmit = (event) => {
   /*Salvando a chave player no local storage com o que o usuario digitar*/
 };
 
+/* Evento para validar o input e o submit do usuário  */
 input.addEventListener("input", validateInput);
 input.addEventListener("submit", handleSubmit);
+
+
+
+/* No geral, aprendi um pouco mais neste projeto sobre interações como setAttribute e removeAttribute. */
